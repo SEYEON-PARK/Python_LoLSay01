9b9bc76224becf74d108b6c022ab49432015ab54
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-06-24 16:57:46 +0900

    makeSound.py

---
+++ makeSound.py
@@ -0,0 +1,23 @@
+from gtts import gTTS
+from playsound import playsound
+
+text ="우리팀이 1점 얻었어요."
+
+tts = gTTS(text=text, lang='ko')
+tts.save("./sound/addOnePoint.mp3")
+playsound("./sound/addOnePoint.mp3")
+
+
+'''
+from gtts import gTTS
+from playsound import playsound
+
+text ="상대팀이 1점 얻었어요."
+text = text.encode('utf-8')
+
+tts = gTTS(text=text, lang='ko')
+tts.speed = 1.8
+tts.save("./sound/addRedTeamOnePoint.mp3")
+playsound("./sound/addRedTeamOnePoint.mp3")
+
+'''
