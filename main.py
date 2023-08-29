85777c4767c4a9affef021130d3842d141d2257e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-08-30 00:09:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -64,21 +64,21 @@ while video.isOpened(): # 비디오가 열려있을 경우 계속 반복!(비디
             text="이겼어요! 축하해요~"
             tts = gTTS(text=text, lang='ko')
             tts.save("./sound/end.mp3") # TTS 음성 파일 생성 및 저장
-            playsound("./sound/good1.mp3")
-            playsound("./sound/end.mp3")
+            playsound("./sound/good1.mp3") # 음성 파일 실행하기
+            playsound("./sound/end.mp3") # 음성 파일 실행하기
             os.remove("./sound/end.mp3") # 만든 파일은 바로 지우기!
         elif(myTeam<redTeam):
             text="졌어요. 아쉬워요."
             tts = gTTS(text=text, lang='ko')
             tts.save("./sound/end.mp3") # TTS 음성 파일 생성 및 저장
-            playsound("./sound/bad1.mp3")
-            playsound("./sound/end.mp3")
+            playsound("./sound/bad1.mp3") # 음성 파일 실행하기
+            playsound("./sound/end.mp3") # 음성 파일 실행하기
             os.remove("./sound/end.mp3") # 만든 파일은 바로 지우기!
         else:
             text="비겼어요. 다음에는 이겨봐요!"
             tts = gTTS(text=text, lang='ko')
             tts.save("./sound/end.mp3") # TTS 음성 파일 생성 및 저장
-            playsound("./sound/end.mp3")
+            playsound("./sound/end.mp3") # 음성 파일 실행하기
             os.remove("./sound/end.mp3") # 만든 파일은 바로 지우기!
         break
 
