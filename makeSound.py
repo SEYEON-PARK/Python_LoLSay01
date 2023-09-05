a4bd92184ecdf2c7662497bd533d0c50b8d8dc7a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-09-06 00:25:23 +0900

    Update makeSound.py

--- makeSound.py
+++ makeSound.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 from playsound import playsound
 
-text ="우리팀이 1점 얻었어요."
+text ="우리팀이 1점 얻었어요." # 텍스트 문자열
 
 tts = gTTS(text=text, lang='ko')
 tts.save("./sound/addOnePoint.mp3")
